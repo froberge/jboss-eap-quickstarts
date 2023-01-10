f85634895f26597ed08872138793fc6407b461f5
Felix Roberge <roberge.felix@gmail.com>
2023-01-09 20:34:03 -0500

    change code to have a postgresql database instead of in memory database

---
+++ kitchensink/src/main/resources/dbscript.sql
@@ -0,0 +1,14 @@
+DROP SEQUENCE IF EXISTS "hibernate_sequence";
+DROP TABLE IF EXISTS "member";
+
+CREATE SEQUENCE hibernate_sequence
+  START WITH     1
+  INCREMENT BY   1;
+
+CREATE TABLE "member"(
+  ID   INT                NOT NULL,
+  NAME VARCHAR(25)       NOT NULL,
+  EMAIL VARCHAR(100) NOT NULL,
+  PHONE_NUMBER  VARCHAR(12) NOT NULL,
+  PRIMARY KEY (ID)
+);
